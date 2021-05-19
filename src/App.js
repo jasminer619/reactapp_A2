fc57c6937df8bea89a7ab20c76fa25cc99bc1532
Jasmine <jasminer619@gmail.com>
2021-05-18 23:20:48 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from "./images/logo.svg"
+import mockup from "./images/illustration-mockups.svg"
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+     
+    <header>
+
+      <img alt="logo" src={logo}/>
+    </header>
+
+    <main>
+      <img alt="mockup" src={mockup}/>
+      <div className ="content">
+        <h2> Build the community your fans will love </h2> 
+        <p> Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion. </p>
+        <button>Register</button>     
+      </div>
+    </main>
+
+    <footer>
+    <i class="fab fa-facebook-f"></i>
+    <i class="fab fa-twitter"></i>
+    <i class="fab fa-instagram"></i>
+    </footer>
+    </>
+  );
+}
+
+export default App;
